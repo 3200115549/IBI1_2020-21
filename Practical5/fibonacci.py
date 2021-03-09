f88f6bf98bc621c98c1553e49a583fa212483125
3200115549 <79949950+3200115549@users.noreply.github.com>
2021-03-09 23:18:10 +0800

    Update fibonacci.py

--- Practical5/fibonacci.py
+++ Practical5/fibonacci.py
@@ -1,4 +1,4 @@
-#first list the number: 1 1 2 3 5 8 13 21 34 55 89 144 243
+#first list the number: 1 1 2 3 5 8 13 21 34 55 89 144 233
 #first time a=0, b=1
 #second time let a=b=1 b=a+b=1
 #so this can repeat for 13 times and print each value of a  
